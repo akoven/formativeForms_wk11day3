71753970216ed0f6c4b415a14c4992ee602a8d64
Attiya <adarensburg@live.com>
2022-03-30 18:18:54 -0500

    phase 3 completed

--- index.js
+++ index.js
@@ -16,8 +16,103 @@ app.get("/", (req, res) => {
 });
 
 app.get('/create', csrfProtection, (req, res, next) => {
-  res.render('create', {user: {}, csrfToken: req.csrfToken()})
+  res.render('create', {user: {}, errors: [], csrfToken: req.csrfToken()})
 })
+
+
+
+
+// const firstNameCheck = (req, res, next) =>{
+//   const {firstName, lastName, email,password,confirmedPassword} = req.body;
+//   if(!req.body.firstName){
+//     req.errors.push('Please provide a first name.')
+//   }
+//   next();
+// }
+
+// const lastNameCheck = (req, res, next) =>{
+//   const {firstName, lastName, email,password,confirmedPassword} = req.body;
+//   if(req.body.lastName.length > 1){
+//     next();
+//   }else{
+//     req.errors.push('Please provide a last name.')
+//   }
+// }
+
+// const emailCheck = (req, res, next) =>{
+//   const {firstName, lastName, email,password,confirmedPassword} = req.body;
+//   if(req.body.email.length > 1){
+//     next();
+//   }else{
+//     req.errors.push('Please provide an email.')
+//   }
+// }
+
+// const passwordCheck = (req, res, next) =>{
+//   const {firstName, lastName, email,password,confirmedPassword} = req.body;
+//   if(req.body.password.length > 1){
+//     next();
+//   }else{
+//     req.errors.push('Please provide a password.')
+//   }
+// }
+
+// const passwordConfirmation = (req, res, next) =>{
+//   const {firstName, lastName, email,password,confirmedPassword} = req.body;
+//   if(req.body.password === req.body.confirmedPassword){
+//     next();
+//   }else{
+//     req.errors.push('The provided values for the password and password confirmation fields did not match.')
+
+//   }
+// }
+
+const validationMiddleware = (req, res, next) => {
+	const { firstName, lastName, email, password, confirmedPassword } = req.body;
+	const errors = [];
+
+	if (!firstName) {
+		errors.push('Please provide a first name.');
+	}
+
+	if (!lastName) {
+		errors.push('Please provide a last name.');
+	}
+
+	if (!email) {
+		errors.push('Please provide an email.');
+	}
+
+	if (!password) {
+		errors.push('Please provide a password.');
+	}
+
+	if (password && password !== confirmedPassword) {
+		errors.push('The provided values for the password and password confirmation fields did not match.');
+	}
+
+	req.errors = errors;
+	next();
+};
+
+
+app.post('/create', validationMiddleware, csrfProtection, (req, res, next) => {
+  const {firstName, lastName, email} =req.body
+
+  if(req.errors.length > 0){
+    res.render('create', {user: req.body, errors: req.errors, csrfToken: req.csrfToken()});
+  }else{
+    const user = {
+      id: users.length+1,
+      firstName,
+      lastName,
+      email
+    }
+    users.push(user);
+    res.redirect('/');
+  }
+})
+
 
 const users = [
   {
