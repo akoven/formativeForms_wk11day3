fde9c7877147f56c740ab3a35bd87e74999bc84f
Anthony Lovern <voltorodarkwatch@gmail.com>
2022-03-30 17:33:27 -0400

    Create User Form Created

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require("express");
 
 const app = express();
+const cookieParser = require('cookie-parser');
+const csrf = require('csurf');
+const csrfProtection = csrf({cookie: true});
 const port = process.env.PORT || 5000;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
   res.render('index', {users});
 });
+
+app.get('/create', csrfProtection, (req, res, next) => {
+  res.render('create', {user: {}, csrfToken: req.csrfToken()})
+})
 
 const users = [
   {
